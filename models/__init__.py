e642a7983dee651bd2c587abf8aa6dbbe3f2e55a
Do Chau Long <dolong2110@gmail.com>
2021-11-19 17:34:40 +0700

    fixbug

--- models/__init__.py
+++ models/__init__.py
@@ -2,19 +2,14 @@ from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 
 def get_model(model_name):
     if model_name == 'resnet18':
-        from resnet import resnet18
         model = resnet18()
     elif model_name == 'resnet34':
-        from resnet import resnet34
         model = resnet34()
     elif model_name == 'resnet50':
-        from resnet import resnet50
         model = resnet50()
     elif model_name == 'resnet101':
-        from resnet import resnet101
         model = resnet101()
     elif model_name == 'resnet152':
-        from resnet import resnet152
         model = resnet152()
     # elif model_name == 'mobilenet':
     #     from mobilenet import mobilenet
