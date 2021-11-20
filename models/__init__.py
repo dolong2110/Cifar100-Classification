6ab74413eb4885fc9d55491ad226fecbca902501
Do Chau Long <dolong2110@gmail.com>
2021-11-21 01:11:15 +0700

    refactor get model code

--- models/__init__.py
+++ models/__init__.py
@@ -1,25 +1,12 @@
 from .basic_nn import Net
 from .resnet import resnet9, resnet18, resnet34, resnet50, resnet101, resnet152
 
-def get_model(model_name):
-    if model_name == 'basic_nn':
-        model = Net()
-    elif model_name == 'resnet9':
-        model = resnet9()
-    elif model_name == 'resnet18':
-        model = resnet18()
-    elif model_name == 'resnet34':
-        model = resnet34()
-    elif model_name == 'resnet50':
-        model = resnet50()
-    elif model_name == 'resnet101':
-        model = resnet101()
-    elif model_name == 'resnet152':
-        model = resnet152()
-    # elif model_name == 'mobilenet':
-    #     from mobilenet import mobilenet
-    #     model = mobilenet()
-    else:
-        raise 'Model {} not available'.format(model_name)
+models_dictionary = {'basic_nn': Net(), 'resnet9': resnet9(), 'resnet18': resnet18(),
+                     'resnet34': resnet34(), 'resnet50': resnet50(), 'resnet101': resnet101(),
+                     'resnet152': resnet152()}
 
+def get_model(model_name):
+    model = models_dictionary.get(model_name)
+    if not model:
+        raise 'Model {} not available'.format(model_name)
     return model
