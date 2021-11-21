5b752f7f47d05cb88d529c070d3267b778bc27b6
Do Chau Long <dolong2110@gmail.com>
2021-11-21 17:44:00 +0700

    fixbug gpu

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from configs import global_configs
 
 def main(args):
     device = devices.get_default_device()
-    return "My device ", device
+    raise "My device " + device
     model = models.get_model(args.model)
 
     data = Dataset(args.image_size)
