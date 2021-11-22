3e827fc77af51939f701400ab998279ff9a5e8aa
Do Chau Long <dolong2110@gmail.com>
2021-11-22 15:26:36 +0700

    data get mean and std

--- dataset/augmentation.py
+++ dataset/augmentation.py
@@ -4,10 +4,16 @@ import torchvision.transforms as tt
 def get_cifar100_mean_std(images) -> (float, float):
     average = torch.Tensor([0, 0, 0])
     standard_dev = torch.Tensor([0, 0, 0])
-    for image in images:
-        print(image[0][1])
-        average += image[0].mean([1, 2])
-        standard_dev += image[0].std([1, 2])
+    # for image in images:
+    #     print(image[0][1])
+    #     average += image[0].mean([1, 2])
+    #     standard_dev += image[0].std([1, 2])
+    # return average / len(images), standard_dev / len(images)
+    for image_genre in images:
+        for image in image_genre[0][1]:
+            print(image)
+            average += image[0].mean([1, 2])
+            standard_dev += image[0].std([1, 2])
     return average / len(images), standard_dev / len(images)
 
 def augment_cifar100(image_resolution, mean, std):
