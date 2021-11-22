837174cc2a58f4fbb5829a98a1da0bb4e7c854f1
Do Chau Long <dolong2110@gmail.com>
2021-11-22 23:50:17 +0700

    data get mean and std

--- dataset/augmentation.py
+++ dataset/augmentation.py
@@ -5,6 +5,7 @@ def get_cifar100_mean_std(images) -> (float, float):
     average = torch.Tensor([0, 0, 0])
     standard_dev = torch.Tensor([0, 0, 0])
     print(len(images))
+    print(len(images[0][0]))
     for image_type in images:
         for image in image_type[0]:
             average += image.mean([1, 2])
