55a5c1e227184b276ce0a31674230a27921ee96d
Do Chau Long <dolong2110@gmail.com>
2021-11-23 06:57:02 +0700

    mobilenet v2

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 from .basic_nn import Net, LinearRegression
 from .resnet import resnet9, resnet18, resnet34, resnet50, resnet101, resnet152
-from .mobilenet import mobilenet
+from .mobilenet import mobilenet, mobilenetv2
 
 models_dictionary = {'basic_nn': Net(), 'linear_regression': LinearRegression(), 'resnet9': resnet9(),
                      'resnet18': resnet18(), 'resnet34': resnet34(), 'resnet50': resnet50(), 'resnet101': resnet101(),
-                     'resnet152': resnet152(), 'mobilenet': mobilenet()}
+                     'resnet152': resnet152(), 'mobilenet': mobilenet(), 'mobilenetv2': mobilenetv2()}
 
 def get_model(model_name):
     model = models_dictionary.get(model_name)
